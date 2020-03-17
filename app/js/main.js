fa308da84849c8417cc0b0ffc60c6ca8a5bdbcfd
skrega <skorpionn440@gmail.com>
2020-03-17 15:23:57 +0300

    added sections and adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,14 @@
 $(function(){
 
+    $('.slider__items').slick({
+        dots: true,
+        arrows: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+    });
+
+    $('.menu__btn').on('click', function () {
+        $('.menu__list').slideToggle();
+    });
+
 });
